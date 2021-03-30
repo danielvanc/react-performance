a033a450ed77a97d4e46f8e3ae6b3d52b38df5df
danielvanc <email@danielvanc.com>
2021-03-30 09:05:30 +0100

    Exercise 1 - complete (edit)!

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,14 @@
 
 import * as React from 'react'
 // 💣 remove this import
-import Globe from '../globe'
+// import Globe from '../globe'
 
 // 🐨 use React.lazy to create a Globe component which using a dynamic import
 // to get the Globe component from the '../globe' module.
-// const Globe = React.lazy(() => import('../globe'))
+const Globe = React.lazy(() => import('../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
-
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
         style={{
@@ -27,11 +22,14 @@ function App() {
           padding: '2rem',
         }}
       >
-        <label style={{marginBottom: '1rem'}}>
+        <label 
+          style={{marginBottom: '1rem'}}
+        >
           <input
             type="checkbox"
             checked={showGlobe}
             onChange={e => setShowGlobe(e.target.checked)}
+
           />
           {' show globe'}
         </label>
@@ -43,8 +41,5 @@ function App() {
       </div>
   )
 }
-// 🦉 Note that if you're not on the isolated page, then you'll notice that this
-// app actually already has a React.Suspense component higher up in the tree
-// where this component is rendered, so you *could* just rely on that one.
 
 export default App
