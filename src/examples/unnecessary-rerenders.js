fd8925e7bd8af1817c0f7172017f9438237cff7f
Daniel Van Cuylenburg <email@danielvanc.com>
2021-04-07 12:04:08 +0100

    Exercise 3 - complete!

--- src/examples/unnecessary-rerenders.js
+++ src/examples/unnecessary-rerenders.js
@@ -6,6 +6,8 @@ function CountButton({count, onClick}) {
   return <button onClick={onClick}>{count}</button>
 }
 
+CountButton = React.memo(CountButton)
+
 function NameInput({name, onNameChange}) {
   return (
     <label>
@@ -13,6 +15,8 @@ function NameInput({name, onNameChange}) {
     </label>
   )
 }
+
+NameInput = React.memo(NameInput);
 
 function Example() {
   const [name, setName] = React.useState('')
