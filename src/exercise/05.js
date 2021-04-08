0badfb2df2ff89f28bbf4f49e857d08c99986103
danielvanc <email@danielvanc.com>
2021-04-08 20:27:33 +0100

    Exercise 5: Extra credit 1 - complete!

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -11,6 +11,7 @@ import {
 } from '../utils'
 
 const AppStateContext = React.createContext()
+const AppUpdateContext = React.createContext()
 
 const initialGrid = Array.from({length: 100}, () =>
   Array.from({length: 100}, () => Math.random() * 100),
@@ -38,18 +39,13 @@ function AppProvider({children}) {
     dogName: '',
     grid: initialGrid,
   })
-  // The Grid and Cell memoization isn't enough because
-  // the below state var get's equality compared after the AppProvider is
-  // re-rendered. So the state var is new each time thus everything
-  // below it, re-renders. 
-  // So by adding useMemo here, we're saying, compare it to the last
-  // if no different, don't re-render if AppProvider is re-rendered
-  // by the force re-render func, etc.
-  const value = React.useMemo(() => [state, dispatch], [state]);
+  // const value = React.useMemo(() => [state], [state]);
   
   return (
-    <AppStateContext.Provider value={value}>
-      {children}
+    <AppStateContext.Provider value={state}>
+      <AppUpdateContext.Provider value={dispatch}>
+        {children}
+      </AppUpdateContext.Provider>
     </AppStateContext.Provider>
   )
 }
@@ -62,8 +58,16 @@ function useAppState() {
   return context
 }
 
+function useAppUpdate() {
+  const context = React.useContext(AppUpdateContext)
+  if (!context) {
+    throw new Error('useAppUpdate must be used within the AppProvider')
+  }
+  return context
+}
+
 function Grid() {
-  const [, dispatch] = useAppState()
+  const dispatch = useAppUpdate()
   const [rows, setRows] = useDebouncedState(50)
   const [columns, setColumns] = useDebouncedState(50)
   const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
@@ -81,7 +85,8 @@ function Grid() {
 Grid = React.memo(Grid)
 
 function Cell({row, column}) {
-  const [state, dispatch] = useAppState()
+  const state = useAppState()
+  const dispatch = useAppUpdate()
   const cell = state.grid[row][column]
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -100,7 +105,8 @@ function Cell({row, column}) {
 Cell = React.memo(Cell)
 
 function DogNameInput() {
-  const [state, dispatch] = useAppState()
+  const state = useAppState()
+  const dispatch = useAppUpdate()
   const {dogName} = state
 
   function handleChange(event) {
@@ -131,12 +137,12 @@ function App() {
   return (
     <div className="grid-app">
       <button onClick={forceRerender}>force rerender</button>
-      <AppProvider>
         <div>
-          <DogNameInput />
-          <Grid />
+          <AppProvider>
+            <DogNameInput />
+            <Grid />
+          </AppProvider>
         </div>
-      </AppProvider>
     </div>
   )
 }
